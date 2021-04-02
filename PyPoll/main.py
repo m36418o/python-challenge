8f29b78ca74af11331b018b5a99a0a7858c2b658
jasonwang <huayu.j.wang@gmail.com>
2021-04-01 20:44:07 -0700

    Halfway complete with PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,64 +4,57 @@ import csv # CSV module for csv read/write
 # CSV file path
 csv_path = r'C:\Users\huayu\Documents\GitHub\python-challenge\PyPoll\Resources\election_data.csv'
 
-# Financial Analysis Function
-def FinancialAnalysis(budget_csv):
+# Election Analysis Function
+def ElectionAnalysis(election_csv):
     '''
     Parameters: 
-        budget_csv (csvfile) - The csv data that will be taken in for analysis.
+        election_csv (csvfile) - The csv data that will be taken in for analysis.
     Return: 
         None
     Description:
-        This function will conduct the financial anlaysis based on the budget_csv file. 
+        This function will conduct the election anlaysis based on the election_csv file. 
     '''
     # Var init
-    months = []
-    changes = []
-    total_list = []
-    increase_date = ""
-    decrease_date = ""
-    increase = 0
-    decrease = 0
+    election_dict = {}
+    dict_keys = []
+    percentage = []
+    total_vote = 0
 
-    # Going trhough each row in budget_csv
-    for row in budget_csv:
-        total_list.append(int(row[1]))
-        months.append(row[0])
+    # Going trhough each row in election_csv
+    for row in election_csv:
+        if row[2] not in election_dict.keys():
+            election_dict[row[2]] = 1
+        else:
+            election_dict[row[2]] += 1
+        total_vote += 1
 
-    # Going through each item in the total_list
-    for i in range(0,len(total_list) - 1):
-        diff = total_list[i + 1] - total_list[i] # Calculate difference of profit between months
-        changes.append(diff)
-        # Conditional to check for max and min of differences
-        if diff > increase: 
-            increase = diff
-            increase_date = months[i + 1]
-        elif diff < decrease:
-            decrease = diff
-            decrease_date = months[i + 1]
-
-    # Calculate average changes between months
-    average = round(sum(changes)/len(changes),2)
+    for key in election_dict.keys():
+        dict_keys.append(key)
+        percentage.append(round(election_dict[key]/total_vote*100, 3))
+    
+    print(percentage[0])
+    
     
     # Printing result to terminal
-    print("Financial Analysis")
-    print("-------------------------------")
-    print(f"Total Months: {len(months)}")
-    print(f"Total: ${sum(total_list)}")
-    print(f"Average Change: ${average}")
-    print(f"Greatest Increase in Profits: {increase_date} (${increase})")
-    print(f"Greatest Decrease in Profits: {decrease_date} (${decrease})")
+#    print("Financial Analysis")
+#    print("-------------------------------")
+#    print(f"Total Months: {len(months)}")
+#    print(f"Total: ${sum(total_list)}")
+#    print(f"Average Change: ${average}")
+#    print(f"Greatest Increase in Profits: {increase_date} (${increase})")
+#    print(f"Greatest Decrease in Profits: {decrease_date} (${decrease})")
 
     # Export result to text file
-    export_file = open(r"C:\Users\huayu\Documents\GitHub\python-challenge\PyBank\Analysis\Financial Analysis.txt", "w")
-    export_file.write("Financial Analysis\n")
-    export_file.write("-------------------------------\n")
-    export_file.write(f"Total Months: {len(months)}\n")
-    export_file.write(f"Total: ${sum(total_list)}\n")
-    export_file.write(f"Average Change: ${average}\n")
-    export_file.write(f"Greatest Increase in Profits: {increase_date} (${increase})\n")
-    export_file.write(f"Greatest Decrease in Profits: {decrease_date} (${decrease})\n")
-    export_file.close()
+#    export_file = open(r"C:\Users\huayu\Documents\GitHub\python-challenge\PyBank\Analysis\Financial Analysis.txt", "w")
+#    export_file.write("Financial Analysis\n")
+#    export_file.write("-------------------------------\n")
+#    export_file.write(f"Total Months: {len(months)}\n")
+#    export_file.write(f"Total: ${sum(total_list)}\n")
+#    export_file.write(f"Average Change: ${average}\n")
+#    export_file.write(f"Greatest Increase in Profits: {increase_date} (${increase})\n")
+#    export_file.write(f"Greatest Decrease in Profits: {decrease_date} (${decrease})\n")
+#    export_file.close()
+    
 
     return None
 
@@ -76,7 +69,7 @@ def main():
         header = next(csvreader, None)
 
         # Pass csvreader into the Financial Analysis Function
-        FinancialAnalysis(csvreader)
+        ElectionAnalysis(csvreader)
 
 if __name__ == "__main__":
     main()
